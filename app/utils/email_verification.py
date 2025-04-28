ea96fb35276193d4c399a71bb43be44cae311c6d
timothysaatum <tsaatum@outlook.com>
2025-04-28 07:34:34 +0000

    change email

--- app/utils/email_verification.py
+++ app/utils/email_verification.py
@@ -5,15 +5,15 @@ def send_verification_email(email: str, token: str):
 
     msg = EmailMessage()
     msg["Subject"] = "Verify your email"
-    msg["From"] = "labconnectltd@gmail.com"
+    msg["From"] = "timothysaatum@gmail.com"
     msg["To"] = email
     msg.set_content(f"Click the link to verify your email:\n\nhttp://localhost:5173/verify-email?token={token}")
 
     # Gmail SMTP configuration
     smtp_server = "smtp.gmail.com"
     smtp_port = 587
-    sender_email = "labconnectltd@gmail.com"
-    app_password = "nkbdtufounzmcmxd"
+    sender_email = "timothysaatum@gmail.com"
+    app_password = "otbeibppuznnveex"
 
     try:
         with smtplib.SMTP(smtp_server, smtp_port) as server:
